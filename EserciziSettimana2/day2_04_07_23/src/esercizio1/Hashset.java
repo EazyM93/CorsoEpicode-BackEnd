48ce12fabc9f418d01efbc88e6d548298589c34e
EazyM93 <centini.manuel.93@gmail.com>
2023-07-04 18:23:32 +0200

    up

--- EserciziSettimana2/day2_04_07_23/src/esercizio1/Hashset.java
+++ EserciziSettimana2/day2_04_07_23/src/esercizio1/Hashset.java
@@ -1,9 +1,7 @@
 package esercizio1;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
@@ -12,17 +10,25 @@ public class Hashset {
 
 	public static void main(String[] args) {
 		
+		// inizializzazione scanner 
 		Scanner scanner = new Scanner(System.in);
+		
+		// creazione array principale
 		Set<String> parole = new HashSet<String>();
+		
+		// creazione array per i duplicati
 		List<String> duplicati = new ArrayList<String>();
  		
 		System.out.print("Numero di elementi da inserire: ");
 		int num = Integer.parseInt(scanner.nextLine());
 		
+		// ciclo per aggiungere gli elementi
 		for(int i = 0; i < num; i++) {
 			
 			System.out.printf("Inserisci elemento %d : ", (i + 1));
 			String parola = scanner.nextLine();
+			
+			// verifica se esiste già il valore ed eventualmente lo mette nei duplicati
 			if(!parole.add(parola)) duplicati.add(parola);
 			
 		}
@@ -34,8 +40,6 @@ public class Hashset {
 		System.out.println();
 		System.out.printf("Numero di parole distinte: %d\n", parole.size());
 		for(String parola: parole) System.out.printf("- %s\n", parola);
-		
-		
 		
 		scanner.close();
 	}
