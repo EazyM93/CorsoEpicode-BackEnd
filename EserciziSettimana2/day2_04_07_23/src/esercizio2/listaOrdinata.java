ce64962fcdaebb5133d203bc98bebb64e0138176
EazyM93 <centini.manuel.93@gmail.com>
2023-07-04 15:44:00 +0200

    up

--- EserciziSettimana2/day2_04_07_23/src/esercizio2/listaOrdinata.java
+++ EserciziSettimana2/day2_04_07_23/src/esercizio2/listaOrdinata.java
@@ -2,17 +2,16 @@ package esercizio2;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-import java.util.TreeSet;
 
 public class listaOrdinata {
 
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		TreeSet<Integer> numbers = new TreeSet<Integer>();
+		List<Integer> numbers = new ArrayList<Integer>();
  		
 		System.out.print("Numero di elementi da generare: ");
 		int num = Integer.parseInt(scanner.nextLine());
@@ -21,23 +20,42 @@ public class listaOrdinata {
 			numbers.add((int)(Math.random() * 100));
 		}
 		
+		Collections.sort(numbers);
+		
 		System.out.println(numbers);
 		
 		System.out.println(reSort(numbers));
 		
+		pariDispari(numbers, true);
+		System.out.println();
+		pariDispari(numbers, false);
+		
 	}
 
-	public static List<Integer> reSort(TreeSet<Integer> lista) {
+	public static List<Integer> reSort(List<Integer> lista) {
 		
 		List<Integer> newList = new ArrayList<Integer>();
+		
 		newList.addAll(lista);
-		newList.addAll(lista.descendingSet());
+		Collections.reverse(lista);
+		newList.addAll(lista);
+		Collections.sort(lista);
 		
 		return newList;
 		
 	}
 	
-//	public static void pariDispari(TreeSet<Integer>, boolean x) {
-//		
-//	}
+	public static void pariDispari(List<Integer> list, boolean x) {
+		
+		if(x == true) {
+			System.out.print("Numeri pari :");
+			for(int valore: list) if(valore % 2 == 0) System.out.print(" " + valore);
+		}
+		
+		if(x == false){
+			System.out.print("Numeri dispari :");
+			for(int valore: list) if(valore % 2 != 0) System.out.print(" " + valore);
+		}
+		
+	}
 }
