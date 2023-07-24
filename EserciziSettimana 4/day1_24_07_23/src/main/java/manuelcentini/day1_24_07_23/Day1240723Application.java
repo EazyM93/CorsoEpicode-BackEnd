59c3c5e1d50c05666bc4ab60997b244c2b850bd9
EazyM93 <centini.manuel.93@gmail.com>
2023-07-24 14:26:17 +0200

    Creazione progetto SpringBoot

---
+++ EserciziSettimana 4/day1_24_07_23/src/main/java/manuelcentini/day1_24_07_23/Day1240723Application.java
@@ -0,0 +1,15 @@
+package manuelcentini.day1_24_07_23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1240723Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day1240723Application.class, args);
+		
+		
+	}
+
+}
---
+++ EserciziSettimana 4/day1_24_07_23/src/test/java/manuelcentini/day1_24_07_23/Day1240723ApplicationTests.java
@@ -0,0 +1,13 @@
+package manuelcentini.day1_24_07_23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day1240723ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
