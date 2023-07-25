70e2d3217dd2f9bb1ce94896ecf9bbbddfbfcbbf
EazyM93 <centini.manuel.93@gmail.com>
2023-07-25 15:34:57 +0200

    Update Ordine.java

--- EserciziSettimana 4/day1_24_07_23/src/main/java/entities/Ordine.java
+++ EserciziSettimana 4/day1_24_07_23/src/main/java/entities/Ordine.java
@@ -1,5 +1,32 @@
 package entities;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import enums.StatoOrdine;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Ordine {
 
+	@Value("${application.coperto}")
+	private int coperto;
+	
+	private int numeroOrdine;
+	private StatoOrdine stato;
+	private int numeroCoperti;
+	private String orario;
+	private Double importoTotale;
+	
+	public Ordine(LocalDateTime orario) {
+		this.orario = orario.getHour() + ":" + orario.getMinute();
+	}
+	
+	
+	
 }
