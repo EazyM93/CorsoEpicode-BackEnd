c867790a51ee9fb1306a114f6ca89e72eabac481
EazyM93 <centini.manuel.93@gmail.com>
2023-07-27 14:55:52 +0200

    Up

---
+++ EserciziSettimana 4/day4_27_07_23/src/main/java/day4_27_07_23/Day4270723Application.java
@@ -0,0 +1,13 @@
+package day4_27_07_23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day4270723Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day4270723Application.class, args);
+	}
+
+}
---
+++ EserciziSettimana 4/day4_27_07_23/src/test/java/day4_27_07_23/Day4270723ApplicationTests.java
@@ -0,0 +1,13 @@
+package day4_27_07_23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day4270723ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
