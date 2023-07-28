22bd2ae3fb4b427ce6449c357431e556dd432ddd
Manuel Centini <centini.manuel.93@gmail.com>
2023-07-28 15:26:50 +0200

    minor update

--- EserciziSettimana 4/gestionePrenotazioni/src/main/java/gestionePrenotazioni/entities/Edificio.java
+++ EserciziSettimana 4/gestionePrenotazioni/src/main/java/gestionePrenotazioni/entities/Edificio.java
@@ -3,11 +3,11 @@ package gestionePrenotazioni.entities;
 import java.util.Set;
 
 import org.springframework.context.annotation.Scope;
-import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
