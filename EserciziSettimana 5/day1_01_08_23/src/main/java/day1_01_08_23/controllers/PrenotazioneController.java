3d13e46b020fc8022eb71961017a45db62f6cbc5
EazyM93 <centini.manuel.93@gmail.com>
2023-08-01 19:19:29 +0200

    Gestione errore prenotazione in un giorno già occupato

--- EserciziSettimana 5/day1_01_08_23/src/main/java/day1_01_08_23/controllers/PrenotazioneController.java
+++ EserciziSettimana 5/day1_01_08_23/src/main/java/day1_01_08_23/controllers/PrenotazioneController.java
@@ -1,9 +1,12 @@
 package day1_01_08_23.controllers;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +16,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import day1_01_08_23.entities.Prenotazione;
-import day1_01_08_23.entities.Utente;
 import day1_01_08_23.payloads.NewPostazioneBody;
 import day1_01_08_23.services.PrenotazioneService;
 
@@ -27,8 +29,18 @@ public class PrenotazioneController {
 	// ---------------------------------------------------------POST loclahost/prenotazioni (+request body)
 	@PostMapping("")
 	@ResponseStatus(HttpStatus.CREATED)
-	public Prenotazione salvaPrenotazione(@RequestBody NewPostazioneBody newBody) {
-		return ps.save(newBody);
+	public Prenotazione salvaPrenotazione(@RequestBody NewPostazioneBody newBody) throws Exception {
+		
+		boolean dataOccupata = false;
+		LocalDate dataCorrente = newBody.getDate();
+		
+		for(Prenotazione p: ps.getPrenotazioni())
+			if(p.getDataPrenotazione().equals(dataCorrente)) {
+				dataOccupata = true;
+				throw new Exception("La postazione è già occupata in questa data");
+			} 
+		
+		return (dataOccupata) ? null : ps.save(newBody);
 	}
 	
 	// ---------------------------------------------------------GET loclahost/prenotazioni
@@ -37,10 +49,17 @@ public class PrenotazioneController {
 		return ps.getPrenotazioni();
 	}
 	
-	// ---------------------------------------------------------GET loclahost/utenti/{id}
+	// ---------------------------------------------------------GET loclahost/prenotazioni/{id}
 	@GetMapping("/{id_prenotazione}")
 	public Prenotazione trovaId(@PathVariable int id_prenotazione) throws Exception {
 		return ps.findById(id_prenotazione)
 				.orElseThrow(() -> new Exception("ID Utente non trovato"));
+	}
+	
+	// ---------------------------------------------------------DELETE loclahost/prenotazioni/{id}
+	@DeleteMapping("/{id_prenotazione}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void findAndDelete(@PathVariable int id_prenotazione) {
+		ps.findByIdAndDelete(id_prenotazione);
 	}
 }
