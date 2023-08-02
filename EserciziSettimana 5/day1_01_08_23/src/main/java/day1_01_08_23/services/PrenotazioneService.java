b99ad07772926f5aa0ebaadbb4bc7fa4d3e9f9c6
EazyM93 <centini.manuel.93@gmail.com>
2023-08-02 10:58:39 +0200

    Update PrenotazioneService.java

--- EserciziSettimana 5/day1_01_08_23/src/main/java/day1_01_08_23/services/PrenotazioneService.java
+++ EserciziSettimana 5/day1_01_08_23/src/main/java/day1_01_08_23/services/PrenotazioneService.java
@@ -10,7 +10,9 @@ import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import day1_01_08_23.entities.Postazione;
 import day1_01_08_23.entities.Prenotazione;
+import day1_01_08_23.entities.Utente;
 import day1_01_08_23.enums.TipologiaPostazione;
 import day1_01_08_23.payloads.NewPostazioneBody;
 
@@ -29,18 +31,20 @@ public class PrenotazioneService {
 	public Prenotazione save(NewPostazioneBody newBody) throws Exception {
 		
 		LocalDate dataCorrente = newBody.getDate();
-		int postazioneCorrente = newBody.getId_postazione();
-		int utenteCorrente = newBody.getId_utente();
+		Postazione postazioneCorrente = ps.findById(newBody.getId_postazione())
+				.orElseThrow(() -> new Exception("Postazione non trovata"));
+		Utente utenteCorrente = us.findById(newBody.getId_utente())
+				.orElseThrow(() -> new Exception("Utente non trovato"));	
 		
 		for(Prenotazione p: getPrenotazioni()) {
 			
 			//------------------------------------------------check postazione data occupata 
-			if(p.getPostazione().getId_postazione() == postazioneCorrente
+			if(p.getPostazione().getId_postazione() == postazioneCorrente.getId_postazione()
 					&& p.getDataPrenotazione().equals(dataCorrente))
 				throw new Exception("La postazione è già occupata in questa data");
 			
 			//------------------------------------------------check utente con più prenotazioni nello stesso giorno
-			if(p.getUtente().getId_utente() == utenteCorrente
+			if(p.getUtente().getId_utente() == utenteCorrente.getId_utente()
 					&& p.getDataPrenotazione().equals(dataCorrente))
 				throw new Exception("L'utente ha già prenotato una postazione in questa data");
 			
@@ -51,16 +55,16 @@ public class PrenotazioneService {
 			throw new Exception("Troppo tardi per prenotare");
 			
 		// costruzione prenotazione dal body
-		Prenotazione p = Prenotazione.builder()
+		Prenotazione prenotazioneCorrente = Prenotazione.builder()
 				.id_prenotazione(Math.abs(new Random().nextInt()))
-				.dataPrenotazione(newBody.getDate())
-				.utente(us.findById(newBody.getId_utente()).get())
-				.postazione(ps.findById(newBody.getId_postazione()).get())
+				.dataPrenotazione(dataCorrente)
+				.utente(utenteCorrente)
+				.postazione(postazioneCorrente)
 				.build();
 		
-		this.prenotazioni.add(p);
+		this.prenotazioni.add(prenotazioneCorrente);
 		
-		return p;
+		return prenotazioneCorrente;
 	}
 	
 	// ----------------------------------------------------ritorna lista di prenotazioni
