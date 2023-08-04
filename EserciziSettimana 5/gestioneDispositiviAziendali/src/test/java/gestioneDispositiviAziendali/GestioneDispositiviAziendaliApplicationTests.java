eca27f2d836724b28d6676ca32201f32a6d334b7
EazyM93 <centini.manuel.93@gmail.com>
2023-08-04 09:55:14 +0200

    Gestione Dispositivi Aziendali - Creazione Progetto

---
+++ EserciziSettimana 5/gestioneDispositiviAziendali/src/test/java/gestioneDispositiviAziendali/GestioneDispositiviAziendaliApplicationTests.java
@@ -0,0 +1,13 @@
+package gestioneDispositiviAziendali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneDispositiviAziendaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
