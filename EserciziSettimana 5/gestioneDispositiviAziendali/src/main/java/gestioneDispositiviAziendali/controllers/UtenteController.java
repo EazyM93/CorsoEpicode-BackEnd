401d39d573b5cb7523512eaf2032b6c54ff3b8bf
EazyM93 <centini.manuel.93@gmail.com>
2023-08-04 13:39:50 +0200

    Creazione metodi utente

--- EserciziSettimana 5/gestioneDispositiviAziendali/src/main/java/gestioneDispositiviAziendali/controllers/UtenteController.java
+++ EserciziSettimana 5/gestioneDispositiviAziendali/src/main/java/gestioneDispositiviAziendali/controllers/UtenteController.java
@@ -1,10 +1,13 @@
 package gestioneDispositiviAziendali.controllers;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,17 +25,30 @@ public class UtenteController {
 	@Autowired
 	UtenteService us;
 	
-	// ---------------------------------------------------------POST loclahost/utenti (+request body)
+	// ---------------------------------------------------------POST utente
 	@PostMapping("")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Utente salvaUtente(@RequestBody Utente bodyUtente) {
 		return us.save(bodyUtente);
 	}
 	
-	// ---------------------------------------------------------GET loclahost/utenti
+	// ---------------------------------------------------------GET utenti
 	@GetMapping("")
 	public List<Utente> visualizzaUtenti(){
 		return us.getUtenti();
 	}
 	
+	// ---------------------------------------------------------GET utente (id)
+	@GetMapping("/{id_utente}")
+	public Utente trovaId(@PathVariable UUID id_utente) throws Exception {
+		return us.findById(id_utente)
+				.orElseThrow(() -> new Exception("ID Utente non trovato"));
+	}
+	
+	// ---------------------------------------------------------DELETE loclahost/utenti/{id}
+	@DeleteMapping("/{id_utente}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void findAndDelete(@PathVariable UUID id_utente) {
+		us.findByIdAndDelete(id_utente);
+	}
 }
--- EserciziSettimana 5/gestioneDispositiviAziendali/src/main/java/gestioneDispositiviAziendali/services/UtenteService.java
+++ EserciziSettimana 5/gestioneDispositiviAziendali/src/main/java/gestioneDispositiviAziendali/services/UtenteService.java
@@ -1,6 +1,9 @@
 package gestioneDispositiviAziendali.services;
 
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,4 +27,26 @@ public class UtenteService {
 	public List<Utente> getUtenti(){
 		return ur.findAll();
 	}
+	
+	// ----------------------------------------------------fetch utente per id
+	public Optional<Utente> findById(UUID id) {
+		Utente utenteTrovato = null;
+		
+		for(Utente u: getUtenti())
+			if(u.getId_utente().equals(id))
+				utenteTrovato = u;
+		
+		return Optional.ofNullable(utenteTrovato);
+ 	}
+	
+	// ----------------------------------------------------cancellazione utente
+	public void findByIdAndDelete(UUID id) {
+		ListIterator<Utente> iterator = getUtenti().listIterator();
+
+		while (iterator.hasNext()) {
+			Utente utenteCorrente = iterator.next();
+			if (utenteCorrente.getId_utente().equals(id)) iterator.remove();
+		}
+	}
+	
 }
