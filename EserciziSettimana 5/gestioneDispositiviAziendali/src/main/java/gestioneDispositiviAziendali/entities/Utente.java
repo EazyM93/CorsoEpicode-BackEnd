fbdfe43a22a1255ee74dd59df1a18ecdb2d19102
EazyM93 <centini.manuel.93@gmail.com>
2023-08-04 10:53:17 +0200

    Update Utente.java

--- EserciziSettimana 5/gestioneDispositiviAziendali/src/main/java/gestioneDispositiviAziendali/entities/Utente.java
+++ EserciziSettimana 5/gestioneDispositiviAziendali/src/main/java/gestioneDispositiviAziendali/entities/Utente.java
@@ -1,6 +1,5 @@
 package gestioneDispositiviAziendali.entities;
 
-import java.util.List;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
@@ -8,7 +7,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
