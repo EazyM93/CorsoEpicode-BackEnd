b60fb934964d0f6b6af004d4e06ccf31c0827705
EazyM93 <centini.manuel.93@gmail.com>
2023-08-04 10:30:23 +0200

    Create Dispositivo.java

---
+++ EserciziSettimana 5/gestioneDispositiviAziendali/src/main/java/gestioneDispositiviAziendali/entities/Dispositivo.java
@@ -0,0 +1,39 @@
+package gestioneDispositiviAziendali.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="dispositivi")
+
+public class Dispositivo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id_dispositivo;
+	
+	@Column(nullable=false, name="tipo")
+	@Enumerated(EnumType.STRING)
+	private TipologiaDispositivo tipo;
+
+	@Column(nullable = false, name = "stato")
+	@Enumerated(EnumType.STRING)
+	private StatoDispositivo stato;
+	
+}
