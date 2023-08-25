a6fee71bd4e3530b3814232c2d3ead14e76285db
EazyM93 <centini.manuel.93@gmail.com>
2023-08-25 12:21:17 +0200

    Create EventQueue.java

---
+++ EserciziSettimana 6/GestionaleIncendi/sondaAlpha/src/main/java/sondaAlpha/utils/EventQueue.java
@@ -0,0 +1,57 @@
+package sondaAlpha.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import sondaAlpha.entities.SensorRe;
+
+@Component
+public class EventQueue {
+	
+	private List<SensorRe> eventList;
+
+    public EventQueue() {
+        eventList = new ArrayList<>();
+    }
+
+    public void addEvent(SensorRe event) {
+        eventList.add(event);
+    }
+
+    public SensorRe removeEvent() {
+        if (!eventList.isEmpty()) {
+            return eventList.remove(0);
+        }
+        return null;
+    }
+
+    public SensorRe peekEvent() {
+        if (!eventList.isEmpty()) {
+            return eventList.get(0);
+        }
+        return null;
+    }
+
+    public boolean containsEvent(SensorRe event) {
+        return eventList.contains(event);
+    }
+
+    public boolean isEmpty() {
+        return eventList.isEmpty();
+    }
+
+    public int size() {
+        return eventList.size();
+    }
+
+    public void clearEvents() {
+        eventList.clear();
+    }
+
+    public List<SensorRe> getAllEvents() {
+        return new ArrayList<>(eventList);
+    }
+
+}
