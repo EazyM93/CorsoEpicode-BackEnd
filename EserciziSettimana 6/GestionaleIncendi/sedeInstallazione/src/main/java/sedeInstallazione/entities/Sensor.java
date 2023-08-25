3b5e8e1b0df559576beb398d766c441b237ef138
EazyM93 <centini.manuel.93@gmail.com>
2023-08-25 11:55:11 +0200

    Create Sensor.java

---
+++ EserciziSettimana 6/GestionaleIncendi/sedeInstallazione/src/main/java/sedeInstallazione/entities/Sensor.java
@@ -0,0 +1,13 @@
+package sedeInstallazione.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Sensor {
+	
+	 private String sensorId;
+	 private int serverPort;
+
+}
